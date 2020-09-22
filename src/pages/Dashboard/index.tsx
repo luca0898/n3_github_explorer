bd41d78f09ed3aacbedff16404633b39fb1b5eaa
Luca Guiraldello <lucaguiraldello@hotmail.com>
2020-09-21 22:12:51 -0300

    feat: creating dashboard and repository routes using react router dom

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return <h1>hello dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return <h1>hello Repository</h1>;
+};
+
+export default Repository;
